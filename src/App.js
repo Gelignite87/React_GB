cd4a5fc13dc6f7118985c3ccbd845a0b900bac77
Gelignite87 <railgun87@rambler.ru>
2022-11-28 10:29:37 +1300

    28.11.2022

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { useState } from 'react';
 import { FormClass } from './components/classes/FormClass';
 import { FormFunc } from './components/func/FormFunc';
+import { Massage } from './components/func/Massage';
 
 export function App() {
   const [toggle, setToggle] = useState(true)
-  const [arr] = useState([{ name: 'Biba' }, { name: 'Boba' },{ name: 'Giga' },{ name: 'Goga' },])
+  const [arr] = useState([{ name: 'Biba' }, { name: 'Boba' }, { name: 'Giga' }, { name: 'Goga' },])
+  const text = "Текст сообщения для домашнего задания"
   return (
     <>
+      <Massage text={text} />
       <FormClass />
       <hr />
       <button onClick={() => setToggle(!toggle)}>{toggle ? 'Hide' : 'Show'}</button>
---
+++ src/components/func/Massage.js
@@ -0,0 +1,7 @@
+export function Massage(props) {
+    return (
+        <>
+            <h1 className='massage'>{props.text}</h1>
+        </>
+    )
+}
